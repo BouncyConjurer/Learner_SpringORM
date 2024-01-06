4ab6989657c902fca1b907cbdf415fa6c9e04540
BouncyConjurer <praveen161298kumar@gmail.com>
2024-01-06 14:23:40 +0530

    New changes

--- src/main/java/com/springorm/dao/StudentDao.java
+++ src/main/java/com/springorm/dao/StudentDao.java
@@ -1,9 +1,11 @@
 package com.springorm.dao;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 
 import com.springorm.entities.Student;
+
+import java.util.List;
 
 public class StudentDao {
 
@@ -12,13 +14,38 @@ public class StudentDao {
 
 
 	//save Student
-	@Transactional
+	@Transactional(readOnly = false)
 	public int insert(Student student) {
 		//insert
 		Integer i =(Integer) this.hibernateTemplate.save(student);
-		return 1;
-		
-	
+		return i;
+	}
+
+	//get single data object
+	public Student show(int studentId){
+
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		return student;
+	}
+
+	//get all the data
+	public List<Student> showAll(){
+
+		List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+		return students;
+	}
+
+	//deleting single data
+	@Transactional
+	public void delete(int studentId){
+		Student student = this.hibernateTemplate.get(Student.class, studentId);
+		this.hibernateTemplate.delete(student);
+	}
+
+	@Transactional
+	public void update(int studentId){
+		this.hibernateTemplate.update(studentId);
+
 	}
 
 	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
--- src/main/java/com/springorm/orm/App.java
+++ src/main/java/com/springorm/orm/App.java
@@ -2,27 +2,23 @@ package com.springorm.orm;
 
 import com.springorm.dao.StudentDao;
 import com.springorm.entities.Student;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        ApplicationContext classPath = new ClassPathXmlApplicationContext("config.xml");
-
+        ClassPathXmlApplicationContext classPath = new ClassPathXmlApplicationContext("config.xml");
 
         StudentDao studentDao = classPath.getBean("studentDao", StudentDao.class);
 
-        Student student = new Student(1,"Praveen Kumar","Kanpur");
+        Student student = new Student(5,"Shareef Kumar",null);
 
         int r = studentDao.insert(student);
 
-        System.out.println("done"+r);
+        System.out.println("done "+r);
+        
+        classPath.close();
 
     }
 }
